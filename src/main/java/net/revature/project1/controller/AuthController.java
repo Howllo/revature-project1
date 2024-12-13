e7afd887e1bcc6a9fe060b4149f6cdb00eb25fc3
Tony <allenr64y@ymail.com>
2024-12-12 19:13:00 -0600

    Finished register mapping.

--- src/main/java/net/revature/project1/controller/AuthController.java
+++ src/main/java/net/revature/project1/controller/AuthController.java
@@ -4,7 +4,9 @@ import net.revature.project1.entity.AppUser;
 import net.revature.project1.enumerator.AuthResult;
 import net.revature.project1.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,28 @@ public class AuthController {
     }
 
     @RequestMapping("/register")
-    public ResponseEntity<?> register(AppUser user){
+    public ResponseEntity<?> register(@RequestBody AppUser user){
         AuthResult result = authService.registration(user);
+        record UserResponse(String username, String displayName){ }
 
+        switch (result){
+            case CREATED -> {
+                return ResponseEntity.status(HttpStatus.CREATED)
+                        .body(new UserResponse(user.getUsername(), user.getDisplayName()));
+            }
+            case EMAIL_TAKEN -> {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already registered");
+            }
+            case USERNAME_TAKEN -> {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already in used");
+            }
+            case INVALID -> {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad email or password");
+            }
+            default -> {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error - " +
+                        "An unexpected error occurred on the server. Please try again later");
+            }
+        }
     }
 }
